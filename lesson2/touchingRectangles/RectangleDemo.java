d0359bbee40e75ad6fede2e69a6b95cbd4fb2c42
Phelan Vendeville <phelan.vendeville@gmail.com>
2015-01-02 10:34:38 -0500

    added rectangle demo code

--- lesson2/touchingRectangles/RectangleDemo.java
+++ lesson2/touchingRectangles/RectangleDemo.java
@@ -12,5 +12,13 @@ public class RectangleDemo
     public static void main(String[] args)
     {
         // TODO: Construct and draw the rectangles
+        double width = 20.0;
+        double height = 30.0;
+
+        Rectangle rec_1 = new Rectangle(60.0, 90.0, width, height);
+        Rectangle rec_2 = new Rectangle(60.0 + width, 90.0 + height, width, height);
+
+        rec_1.draw();
+        rec_2.draw();
     }
 }
